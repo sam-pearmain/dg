388684ee7dc10c76503585acc2c669db9e25350f
sam-pearmain <sam.pearmain@kcl.ac.uk>
2025-05-28 12:55:48 +0100

    slight bug on the init script

--- scripts/initialise.sh
+++ scripts/initialise.sh
@@ -1,9 +1,9 @@
 if nvidia-smi -L > /dev/null 2>&1; then 
-    export JAX_PLATFORM_NAME = "gpu"
+    export JAX_PLATFORM_NAME="gpu"
 else 
-    export JAX_PLATFORM_NAME = "cpu"
+    export JAX_PLATFORM_NAME="cpu"
 fi
 
-export JAX_ENABLE_X64 = "true"
+export JAX_ENABLE_X64="true"
 
 echo "JAX_PLATFORM_NAME: $JAX_PLATFORM_NAME"
