73644e920c55bed2c76c8197427d77dd0d57ea67
sam-pearmain <sam.pearmain@kcl.ac.uk>
2025-05-30 12:25:38 +0100

    added timestepping

--- src/physics/base.py
+++ src/physics/base.py
@@ -1,4 +1,17 @@
 from abc import ABC, abstractmethod
+from enum import Enum
 
 class Physics(ABC):
-    pass
+    """The physics abstract base class is the skeleton for any weak DG formulation of the governing PDE"""
+    def __init__(self):
+        super().__init__()
+
+    @abstractmethod
+    class StateVariables(Enum):
+        pass
+
+    @abstractmethod
+    class AdditionalVariables(Enum):
+        pass
+
+    
