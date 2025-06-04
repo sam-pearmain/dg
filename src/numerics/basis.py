b309fe59c95cdca82cc2b1f23d3cdc32b12b0f13
sam-pearmain <sam.pearmain@kcl.ac.uk>
2025-06-04 17:57:43 +0100

    adding stuff

--- src/numerics/basis.py
+++ src/numerics/basis.py
@@ -1,58 +1,58 @@
 import numpy.polynomial
 
-from abc import ABC, abstractmethod
 from enum import Enum, auto
 from jax import Array
 from meshing.element import ElementType
 from numerics.quadrature import QuadratureType
 from numerics.quadrature.rules import gauss_lobatto_rule, gauss_legendre_rule
-from utils.error import *
+from utils import todo, NotSupportedError
 
 class BasisType(Enum):
     Lagrange = auto()
     Legendre = auto()            
 
-class ReferenceElement(Enum):
+class RefElem(Enum):
     """A reference element enum"""
-    RefPoint = auto()
-    RefLine  = auto()
-    RefQuad  = auto()
-    RefCube  = auto()
-    RefTri   = auto()
-    RefTetra = auto()
+    Point = auto()
+    Line  = auto()
+    Quad  = auto()
+    Cube  = auto()
+    Tri   = auto()
+    Tetra = auto()
 
     def bounds(self) -> tuple[tuple[float, ...], ...]:
         """Returns the bounds of the bounding box for the given reference element"""
         match self:
-            case self.RefPoint: return 0
-            case self.RefLine:  return (-1.0, 1.0)
-            case self.RefQuad:  return ((-1.0, -1.0), (1.0, 1.0))
-            case self.RefCube:  return ((-1.0, -1.0, -1.0), (1.0, 1.0, 1.0))
-            case self.RefTri:   return ((0.0, 0.0), (1.0, 1.0))
-            case self.RefTetra: return ((0.0, 0.0, 0.0), (1.0, 1.0, 1.0))
+            case self.Point: return 0
+            case self.Line:  return (-1.0, 1.0)
+            case self.Quad:  return ((-1.0, -1.0), (1.0, 1.0))
+            case self.Cube:  return ((-1.0, -1.0, -1.0), (1.0, 1.0, 1.0))
+            case self.Tri:   return ((0.0, 0.0), (1.0, 1.0))
+            case self.Tetra: return ((0.0, 0.0, 0.0), (1.0, 1.0, 1.0))
 
     def dimensions(self) -> int:
         """Returns the number of dimensions for the given reference element"""
         match self: 
-            case self.RefPoint: return 0
-            case self.RefLine:  return 1
-            case self.RefQuad:  return 2
-            case self.RefTri:   return 2
-            case self.RefCube:  return 3
-            case self.RefTetra: return 3
+            case self.Point: return 0
+            case self.Line:  return 1
+            case self.Quad:  return 2
+            case self.Tri:   return 2
+            case self.Cube:  return 3
+            case self.Tetra: return 3
 
     def n_dofs(self, order: int) -> int:
         """Returns the number of degrees of freedom for the reference element"""
         match self:
-            case self.RefPoint: return 1
-            case self.RefLine:  return order + 1 
-            case self.RefQuad:  return (order + 1)**2
-            case self.RefTri:   return (order + 1) * (order + 1) // 2
-            case self.RefCube:  return (order + 1)**3
-            case self.RefTetra: return (order + 1) * (order + 2) * (order + 3) // 6
+            case self.Point: return 1
+            case self.Line:  return order + 1 
+            case self.Quad:  return (order + 1)**2
+            case self.Tri:   return (order + 1) * (order + 1) // 2
+            case self.Cube:  return (order + 1)**3
+            case self.Tetra: return (order + 1) * (order + 2) * (order + 3) // 6
 
-    def get_quadrature_points_weights(self) -> tuple[Array, Array]:
-        pass
+    def get_quadrature_points_weights(self, quadrature_type: QuadratureType) -> tuple[Array, Array]:
+        match self:
+            case _: raise todo("finish this")
 
     def get_interpolation_points(self, order: int, method: str) -> Array:
         pass
@@ -66,7 +66,7 @@ class ReferenceElement(Enum):
     def get_gauss_legendre_points(self, order: int) -> Array:
         pass
 
-def eval_lagrange_basis(ref_elem: ElementType, x_q: Array) -> Array:
+def eval_lagrange_basis(ref_elem: RefElem, x_q: Array) -> Array:
     """Evaluate the Lagrange basis function across a given reference element at points x_q, typically the quadrature points"""
     match ref_elem:
         case ElementType.Vertex:        return 0
--- src/solver/solver.py
+++ src/solver/solver.py
@@ -2,10 +2,10 @@ import jax.numpy as jnp
 
 from typing import Union, Dict
 from logging import Logger
-from src.physics import Physics
-from src.meshing import Mesh
-from src.utils import *
-from src.numerics.timestepping.integrator import Integrator
+from physics import Physics
+from meshing import Mesh
+from utils import *
+from numerics.timestepping.integrator import Integrator
 
 class SolverSettings():
     def __init__(
