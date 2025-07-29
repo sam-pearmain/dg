8c7f6f9a06f5d3de6bf874e23244c5457afe2495
sam-pearmain <sam.pearmain@kcl.ac.uk>
2025-07-29 15:40:47 +0100

    fleshing out physics base

--- src/physics/base.py
+++ src/physics/base.py
@@ -1,22 +1,90 @@
 import jax.numpy as jnp 
 
-from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod, abs
 from enum import Enum
+from jax import Array
 
 class Physics(ABC):
-    """The physics abstract base class is the skeleton for any weak DG formulation of the governing PDE"""
+    """
+    The physics abstract base class is the skeleton for any weak DG formulation of a governing 
+    advection-diffusion-type PDE
+    """
     def __init__(self):
-        super().__init__()
-
-    @abstractmethod
-    class StateVariables(Enum):
         pass
 
     @property
-    def n_state_vars(self):
-        """Returns the number of state vars of the system"""
-        len(self.StateVariables)
+    @abstractmethod
+    class StateVariables(Enum):
+        """The state variables"""
+        pass
 
-    def initial_condition(self, x):
-        """Returns the initial conditions at a given coordinate"""
-        return jnp.zeros(self.n_state_vars)
+    @property
+    def n_state_vars(self) -> int:
+        """Returns the number of state vars of the system"""
+        return len(self.StateVariables)
+
+    @property
+    @abstractmethod
+    def dimensions(self) -> int:
+        """The physical dimensions of the system"""
+        pass
+
+    @abstractmethod
+    def compute_convective_flux(
+        self,
+        u: Array
+    ) -> Array:
+        """Computes the analytical convective flux"""
+        pass
+
+    @abstractmethod
+    def compute_diffusive_flux(
+        self,
+        u: Array, 
+        grad_u: Array
+    ) -> Array:
+        """Computes the analytical diffusive flux"""
+        pass
+
+    @abstractmethod
+    def compute_convective_numerical_flux(
+        self,
+        u_l: Array, 
+        u_r: Array, 
+        normal: Array
+    ) -> Array:
+        """Computes the convective numerical flux at either inteior or boundary faces"""
+        pass
+
+    @abstractmethod
+    def compute_diffusive_numerical_flux(
+        self,
+        u_l: Array, 
+        u_r: Array, 
+        grad_u_l: Array, 
+        grad_u_r: Array, 
+        normal: Array
+    ) -> Array:
+        """Computes the diffusive numerical flux on interior faces"""
+        pass 
+
+    @abstractmethod
+    def compute_diffusive_numerical_flux_boundary(
+        self,
+        u_l: Array, 
+        u_r: Array, 
+        grad_u_l: Array, 
+        normal: Array
+    ) -> Array:
+        """Computes the diffusive numerical flux on boundary faces"""
+        pass
+
+    @abstractmethod
+    def compute_source_terms(
+        self,
+        u: Array, 
+        x: Array,
+        t: float,
+    ) -> Array:
+        """Computes the sum of all source terms"""
+        pass
