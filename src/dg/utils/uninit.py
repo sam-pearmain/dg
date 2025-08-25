d063c730fb5ce2b5bf7967f7223a4820f8aaca1d
sam-pearmain <sam.pearmain@kcl.ac.uk>
2025-08-25 16:10:43 +0100

    learning python classes god

--- src/dg/utils/uninit.py
+++ src/dg/utils/uninit.py
@@ -1,48 +1,21 @@
-from typing import Any
-
 class Uninit:
-    def __str__(self):
+    def __repr__(self) -> str:
         return "<uninit>"
-
-    def __repr__():
-        return "<uninit>"
-        
-    def __call__(self, *args, **kwds):
-        self.uninit()
-
-    def __add__(self, _):
-        self.uninit()
-
-    def __radd__(self, _):
-        self.uninit()
-
-    def __sub__(self, _):
-        self.uninit()
-
-    def __rsub__(self, _):
-        self.uninit()
-
-    def __mul__(self, _):
-        self.uninit()
-
-    def __rmul__(self, _):
-        self.uninit()
-
-    def __truediv__(self, _):
-        self.uninit()
-
-    def __rtruediv__(self, _):
-        self.uninit()
-
-    def __float__(self):
-        self.uninit()
-
-    def __int__(self):
-        self.uninit()
-
-    def _uninit():
-        raise ValueError("attempted to use uninitialised object/value")
     
-def isuninit(any: Any) -> bool:
-    """Just a wrapper for isinstance but to check if something is uninitialised"""
-    return isinstance(any, Uninit)
+    def __bool__(self) -> bool:
+        return False
+    
+    def isuninit(self) -> bool:
+        return True
+
+def isuninit(obj: object):
+    return isinstance(obj, Uninit)
+
+def tests():
+    class ABC:
+        a: Uninit = Uninit()
+    
+    print(isuninit(ABC.a))
+
+if __name__ == "__main__":
+    tests()
