200226fc4e514574782e2e5af73145b1c5742ca5
sam-pearmain <sam.pearmain@kcl.ac.uk>
2025-08-26 12:38:39 +0100

    flux

--- src/dg/physics/flux.py
+++ src/dg/physics/flux.py
@@ -11,23 +11,35 @@ if TYPE_CHECKING:
     from dg.physics.pde import PDE
     from dg.physics.interfaces import InterfaceType, Interfaces
 
+class _Convective:
+    """A marker trait for convective terms"""
+    def is_convective(self) -> bool: return True
+
+class _Diffusive:
+    """A marker trait for diffusive terms"""
+    def is_diffusive(self) -> bool: return True
+
 T = TypeVar('T')
 class _Function(ABC, Generic[T]): # type: ignore
+    """A generic function requiring a jit-compiled __call__ method"""
     @jit
     @abstractmethod
     def __call__(self, *args: Any, **kwds: Any) -> Any: ...
 
 P = TypeVar('P', bound = "PDE")
 class _NumericalFluxFunction(_Function[P]):
+    @property
+    @abstractmethod
+    def _DEFINED_ON_INTERFACE(self) -> InterfaceType[P]: ...
+
     @jit
     @abstractmethod
     def __call__(self, physics: P, *args: Any, **kwds: Any) -> Any: ...
 
-    @abstractmethod
-    def defined_on(self) -> InterfaceType: ... 
+    def defined_on(self) -> InterfaceType[P]: return self._DEFINED_ON_INTERFACE 
 
 P = TypeVar('P', bound = "PDE")
-class ConvectiveAnalyticalFlux(_Function[P]):
+class ConvectiveAnalyticalFlux(_Function[P], _Convective):
     @jit
     @abstractmethod
     def __call__(
@@ -38,7 +50,7 @@ class ConvectiveAnalyticalFlux(_Function[P]):
         ...
     
 P = TypeVar('P', bound = "PDE")
-class DiffusiveAnalyticalFlux(_Function[P]):
+class DiffusiveAnalyticalFlux(_Function[P], _Diffusive):
     @jit
     @abstractmethod
     def __call__(
@@ -86,6 +98,19 @@ class _FluxDispatch(ABC, Generic[P, N]):
     @abstractmethod
     def get_numerical_flux_function_on(self, interface: InterfaceType[P]) -> N: 
         return self._dispatch[interface]
+    
+    def sanity_check(self) -> None:
+        if not self._dispatch:
+            raise AttributeError("no flux dispatch defined")
+        
+        for interface_type, numerical_flux_function in self._dispatch.items():
+            if numerical_flux_function.defined_on() != interface_type:
+                raise AttributeError(
+                    f"mismatch interface and numerical flux function: \n"
+                    f"  numerical flux: {numerical_flux_function} \n"
+                    f"  defined on: {numerical_flux_function.defined_on()} \n"
+                    f"  but assigned to {interface_type}"
+                )
 
 P = TypeVar('P', bound = "PDE")
 class ConvectiveNumericalFluxDispatch(_FluxDispatch[P, ConvectiveNumericalFlux[P]]):
@@ -109,19 +134,20 @@ class DiffusiveNumericalFluxDispatch(_FluxDispatch[P, DiffusiveNumericalFlux[P]]
     def get_numerical_flux_function_on(self, interface: InterfaceType[P]) -> DiffusiveNumericalFlux[P]:
         return super().get_numerical_flux_function_on(interface)
 
+P = TypeVar('P', bound = "PDE")
 @compose(autorepr, immutable)
-class Flux(ABC):
-    _convective_analytical_flux:         Optional[ConvectiveAnalyticalFlux]
-    _diffusive_analytical_flux:          Optional[DiffusiveAnalyticalFlux]
-    _convective_numerical_flux_dispatch: Optional[ConvectiveNumericalFluxDispatch]
-    _diffusive_numerical_flux_dispatch:  Optional[DiffusiveNumericalFluxDispatch]
+class Flux(ABC, Generic[P]):
+    _convective_analytical_flux:         Optional[ConvectiveAnalyticalFlux[P]]
+    _diffusive_analytical_flux:          Optional[DiffusiveAnalyticalFlux[P]]
+    _convective_numerical_flux_dispatch: Optional[ConvectiveNumericalFluxDispatch[P]]
+    _diffusive_numerical_flux_dispatch:  Optional[DiffusiveNumericalFluxDispatch[P]]
 
     def __init__(
             self, 
-            convective_analytical_flux: Optional[ConvectiveAnalyticalFlux],
-            diffusive_analytical_flux:  Optional[DiffusiveAnalyticalFlux], 
-            convective_numerical_flux_dispatch:  Optional[ConvectiveNumericalFluxDispatch], 
-            diffusive_numerical_flux_dispatch:   Optional[DiffusiveNumericalFluxDispatch],
+            convective_analytical_flux: Optional[ConvectiveAnalyticalFlux[P]],
+            diffusive_analytical_flux:  Optional[DiffusiveAnalyticalFlux[P]], 
+            convective_numerical_flux_dispatch:  Optional[ConvectiveNumericalFluxDispatch[P]], 
+            diffusive_numerical_flux_dispatch:   Optional[DiffusiveNumericalFluxDispatch[P]],
         ) -> None:
         self._convective_analytical_flux = convective_analytical_flux
         self._diffusive_analytical_flux  = diffusive_analytical_flux
@@ -131,7 +157,7 @@ class Flux(ABC):
         super().__init__()
 
     @abstractmethod
-    def interfaces(self) -> Interfaces: ...
+    def interfaces(self) -> Interfaces[P]: ...
 
     @abstractmethod
     def has_convective_terms(self) -> bool: ...
@@ -140,4 +166,12 @@ class Flux(ABC):
     def has_diffusive_terms(self) -> bool: ... 
 
     def _sanity_check(self) -> None: 
-        pass
+        if self.has_convective_terms() and not self._convective_analytical_flux:
+            raise AttributeError(
+                "flux was marked to have convective terms but convective analytical flux not given"
+            )
+
+        if self.has_diffusive_terms() and not self._diffusive_analytical_flux:
+            raise AttributeError(
+                "flux was marked to have diffusive terms but diffusive analytical flux not given"
+            )
